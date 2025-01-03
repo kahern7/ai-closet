6b9166b9122131913442a3a523cd369babf32837
DESKTOP-4K30N9D\Darren <dreinzy@gmail.com>
2025-01-03 12:50:28 +0000

    Cleaned up main file

--- main.py
+++ main.py
@@ -5,20 +5,3 @@ if __name__ == "__main__":
     root = tk.Tk()
     app = ClosetOptimiserGUI(root)
     root.mainloop()
-
-    # percentages = {
-    #     "drawers": 30,
-    #     "hanging": 50,
-    #     "shelves": 20
-    # }
-
-    # # set max size for each component (inches)
-    # max_size = {
-    #     "drawers": 10,
-    #     "hanging": 20,
-    #     "shelves": 5
-    # }
-
-    # designer = ClosetDesigner()
-    # arrangement = designer.design_closet(percentages)
-    # designer.visualise_closet(arrangement)
